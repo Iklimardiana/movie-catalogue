8f94e25c70829419323e37d283709daf17755207
iklimardiana911@gmail.com <iklimardiana911@gmail.com>
2023-05-07 12:45:44 +0700

    Initialize movie catalogue automated testing implemented

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,13 @@
+const CONFIG = {
+  KEY: 'ea61b0d7ccd5e45aac0d87ac96e673d3',
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+  DEFAULT_LANGUAGE: 'en-us',
+  CACHE_NAME: new Date().toISOString(),
+  DATABASE_NAME: 'movie-catalogue-database',
+  DATABASE_VERSION: 1,
+  OBJECT_STORE_NAME: 'movies',
+  WEB_SOCKET_SERVER: 'wss://movies-feed.dicoding.dev',
+};
+
+export default CONFIG;
